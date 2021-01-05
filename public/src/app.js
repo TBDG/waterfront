a81b7617d27fc56846585bcf649016ca1ac98a78
Joshyray <developer@baert.io>
2021-01-04 18:28:05 -0700

    test deploy

--- public/src/app.js
+++ public/src/app.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React Yo!
+                    Learn React test
                 </a>
             </header>
         </div>

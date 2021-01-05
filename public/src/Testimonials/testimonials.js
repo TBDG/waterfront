8cf40d0b54768f33bb0d00d6106599264d0b01a3
Harrison King <authorharrisonking@gmail.com>
2021-01-05 16:34:22 -0700

    setting up new component and wrote the quotes

---
+++ public/src/Testimonials/testimonials.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Testimonials = () => {
+    const quotes = [
+        [
+            "The Waterfront is a welcome addition for fishkeepers. Louis is very knowledgeable and approachable.\n\nThe thing I like the best about The Waterfront is that Louis is what I call \"The Fish Detective.\" He has been able to track down hard to find plecos for me, quarantine them in his store, make sure they are eating, etc.\n\nI will definitely be a repeat customer.","Doug Jessop"
+        ],[
+            "Amazing fish store!! Definitely recommend! The owner has the best customer service skills out of all the other fish stores in the valley! And super knowledgeable! Thank you Waterfront for helping us with all our needs and our many questions!","Cortnee-jo Mills"
+        ],[
+            "What an amazing experience today at your awesome shop! thank you so much for the time spent with my wife and I, you have definitely gained long lasting customers. I HIGHLY recommend Waterfront as your new LFS. We will see you again soon our new friend!! Cant't wait to see what's next!!", "Beau Mills"
+        ]
+    ]
+
+    
+}
--- public/src/app.js
+++ public/src/app.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import Footer from './Footer/footer.js';
+import Testimonials from './Testimonials/testimonials.js'
 import './app.scss';
 
 function App() {
     return (
         <div className="App">
             <h1>filler</h1>
+            <Testimonials />
             <Footer />
         </div>
     );

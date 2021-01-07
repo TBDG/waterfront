6c106a8082237c973bed5bd97374a720a5984a5e
AvocadoCasualty <baibeebambi@gmail.com>
2021-01-07 14:21:10 -0700

    Updating to current pull

--- public/src/shared/header/header.js
+++ public/src/shared/header/header.js
@@ -1,11 +1,11 @@
 import {useState} from 'react';
-import logo from "https://cdn.shopify.com/s/files/1/0397/0940/9439/files/Water_front_logo_300x300.png?v=1606259958"
 
 function Header() {
     return (
         <div className="Header">
             <div className="logo">
-                <img src={logo}/>
+                Logo Pants
+                {/*<img src={logo}/>*/}
             </div>
             <div className="icons"></div>
             <div className="menu"></div>

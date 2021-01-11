9dbd52fc14cb214db72df17429fe8bd9ca372fcb
Harrison King <authorharrisonking@gmail.com>
2021-01-11 16:53:07 -0700

    commiting before merge prod into this branch

--- public/src/shared/landing.js
+++ public/src/shared/landing.js
@@ -5,7 +5,15 @@ const Landing = () => {
 
     return (
         <div id='landing'>
-            Hello Maurice
+            <div id='landing-text'>            
+                <p className='title'>Utah's Newest Fish Store</p>
+                <div className='subtitle'>
+                    <p>Welcome to Utah's newest fish store, where we specialize in Freshwater fish, plants, and other aquarium supplies.</p>
+                    <p>Come on in and ask our knowledgable staff about what we have in stock or what we can order in for you to make your tank just that much better!</p>
+                </div>
+            </div>
+
+
         </div>
     )
 }

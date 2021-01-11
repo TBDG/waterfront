bf11c76e543fa4f0831fa8ebd9f5d0ab29d32469
Harrison King <authorharrisonking@gmail.com>
2021-01-11 11:36:54 -0700

    initial set up

--- public/src/app.js
+++ public/src/app.js
@@ -1,10 +1,12 @@
 import Footer from './shared/footer.js';
 import Testimonials from './shared/testimonials.js';
 import './app.scss';
+import Landing from './shared/landing.js';
 
 function App() {
     return (
         <div className="App">
+            <Landing />
             <h1>filler</h1>
             <Testimonials />
             <Footer />
---
+++ public/src/shared/landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Landing = () => {
+
+    return (
+        <div id='landing'>
+            Hello Maurice
+        </div>
+    )
+}
+
+export default Landing;

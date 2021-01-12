cc585af320f7cf5ed20507cdd5e65be2162df68d
Harrison King <authorharrisonking@gmail.com>
2021-01-12 07:32:57 -0700

    addressed requested changes

--- public/src/app.js
+++ public/src/app.js
@@ -1,9 +1,10 @@
+import './app.scss';
+
+import Footer from './shared/footer.js';
 import Landing from './shared/landing.js';
+import Location from './shared/location.js'
 import Newsletter from './shared/newsletter';
 import Testimonials from './shared/testimonials.js';
-import Location from './shared/location.js'
-import Footer from './shared/footer.js';
-import './app.scss';
 
 function App() {
     return (

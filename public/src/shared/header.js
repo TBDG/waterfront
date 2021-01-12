ca820ed9b957b94ac56e8b01adc5b96aad18645d
AvocadoCasualty <baibeebambi@gmail.com>
2021-01-12 12:28:26 -0700

    Header complete

--- public/src/shared/header.js
+++ public/src/shared/header.js
@@ -1,17 +1,23 @@
+import {useState} from 'react';
 import logo from '../wf-logo-web.png'
-import {GiHamburgerMenu, GrFormClose} from "react-icons/all";
+import {FiMenu, IoClose} from "react-icons/all";
 
 function Header() {
+    const [navMenu, setNavMenu] = useState(true)
+
 
     const showMenu = () => {
         var x = document.getElementById('menu')
+            x.style.height = '92px'
+            setNavMenu(false)
+    }
+    const hideMenu = () => {
+        var x = document.getElementById('menu')
         if (x.style.height === '92px') {
             x.style.height = '0px'
-        } else {
-            x.style.height = '92px'
+            setNavMenu(true)
         }
     }
-
     return (
         <div className="Header row">
             <div className="logo col-6 col-xl-4">
@@ -20,15 +26,21 @@ function Header() {
                 </a>
             </div>
             <div className="icons col-6">
-                    <GiHamburgerMenu
-                        classname='hamburger'
-                        onClick={showMenu}>
-                    </GiHamburgerMenu>
+                {navMenu ? <FiMenu
+                    className='hamburger'
+                    onClick={showMenu}>
+                </FiMenu> : <div className='closeIcon'><IoClose
+                    className='close'
+                    onClick={hideMenu}/>
+                </div>}
+
             </div>
 
             <div
                 className="menu col-12 col-xl-4"
-                id='menu'>
+                id='menu'
+                onBlurCapture={hideMenu}
+            >
                 <ul>
                     <li><a href='#'>Home</a></li>
                     <li><a href='#'>Live Fish</a></li>
